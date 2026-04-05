d52224f4210985bfbd187da781b097cf166dd31f
yenlinh <linh.ty.63cntt@ntu.edu.vn>
2026-04-06 00:08:36 +0700

    hoan thien bai fragment dong su dung fragment transaction de them fragment

---
+++ BaiTH12.FragmentDong/app/src/main/java/tiil/edu/fragmentdong/MainActivity.java
@@ -0,0 +1,30 @@
+package tiil.edu.fragmentdong;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.FragmentManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+                .add(R.id.frameContent, new ContentFragment() )
+                .add(R.id.frameFooter,new FooterFragment())
+                .commit();
+    }
+}
