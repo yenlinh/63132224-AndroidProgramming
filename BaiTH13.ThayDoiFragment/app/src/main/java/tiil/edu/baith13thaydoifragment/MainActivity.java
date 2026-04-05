6942aff7039670a56f8b40964d02081767e869b4
yenlinh <linh.ty.63cntt@ntu.edu.vn>
2026-04-06 00:15:46 +0700

    hoan thien bai thay doi fragment su dung replace fragment bang su kien

---
+++ BaiTH13.ThayDoiFragment/app/src/main/java/tiil/edu/baith13thaydoifragment/MainActivity.java
@@ -0,0 +1,29 @@
+package tiil.edu.baith13thaydoifragment;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.FragmentManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+                .add(R.id.fragmentContainer_Content, new ContentFragment())
+                .add(R.id.fragmentContainer_Footer,new FooterFragment()).commit();
+    }
+}
